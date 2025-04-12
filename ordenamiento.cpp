a452a3d30b634ccc0523cafde6049fffc7f55cda
Andre Revilla <andre.revilla@ucsp.edu.pe>
2025-04-12 07:56:37 -0500

    Create ordenamiento.cpp

---
+++ ordenamiento.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A[]={2,4,6,1,3,5};
+    int tam=sizeof(A)/sizeof(A[0]);
+    int *p=A;
+    int *q=&A[tam/2];
+
+    for (int *r = A; r < A + tam / 2; r++) {
+        p=r;
+        while (p<q) {
+            if (*p > *q) {
+                int temp = *p;
+                *p = *q;
+                *q = temp;
+            }
+            p++;
+        }
+        q++;
+    }
+    
+    for (int *r = A; r < A + tam; r++) {
+        cout << *r << " ";
+    }
+    
+    return 0;
+}
