ef1c2c8a06b845fc9a3be5dfc20034d2f81d7d4b
Andre Revilla <andre.revilla@ucsp.edu.pe>
2025-09-13 05:28:02 -0500

    Update cubo.cpp

--- cubo.cpp
+++ cubo.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int M[3][3][3] ={{{1,2,3},{4,5,6},{7,8,9}},{{10,11,12},{13,14,15},{16,17,18}},{{0,0,0},{0,0,0},{0,0,0}}};
-
+void multiplicacion(int (*M)[3][3]){
     int *p0 = **M;
     int *p1 = **M + 9;
     int *p2 = **M + 18;
     int n=0;
-
+    
     for (int *p = p0; p < p0 + 3; p++) {
         int distancia1 = p - p0;
         for (int *q = p1; q < p1 +3; q++){
@@ -18,12 +15,19 @@ int main()
             n++;
         }
     }
-
+    
     for (int *p = **M + 18; p < **M + 27; p++) {
         cout << *p << " ";
         if ((p - (**M + 18) + 1) % 3 == 0)
             cout << endl;
     }
+}
+
+int main()
+{
+    int M[3][3][3] ={{{1,2,3},{4,5,6},{7,8,9}},{{10,11,12},{13,14,15},{16,17,18}},{{0,0,0},{0,0,0},{0,0,0}}};
+
+    multiplicacion(M);
 
     return 0;
 }
