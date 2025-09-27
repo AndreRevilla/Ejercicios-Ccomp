924da66ac4b8fde23f634fc164824c6467ac2fc4
Andre Revilla <andre.revilla@ucsp.edu.pe>
2025-09-27 08:10:47 -0500

    Update cubo.cpp

--- cubo.cpp
+++ cubo.cpp
@@ -7,13 +7,12 @@ void multiplicacion(int (*M)[3][3]){
     int *p2 = **M + 18;
     int n=0;
     
-    for (int *p = p0; p < p0 + 3; p++) {
-        int distancia1 = p - p0;
-        for (int *q = p1; q < p1 +3; q++){
-            int distancia2 = q - p1;
-            *(p2+n) = (*(p0 + distancia1*3)) * (*(p1 + distancia2)) + (*(p0 + (distancia1*3) + 1)) * (*(p1 + distancia2 + 3)) + (*(p0 + (distancia1*3) + 2)) * (*(p1 + distancia2 + 6));
+    for (int *p = p0; p < p1; p+=3) {
+        for (int *q = p1; q < p1+3; q++){
+            *(p2+n) = (*(p + 0) * (*(q + 0))) + (*(p + 1) * (*(q + 3))) + (*(p + 2) * (*(q + 6)));
             n++;
         }
+        
     }
     
     for (int *p = **M + 18; p < **M + 27; p++) {
